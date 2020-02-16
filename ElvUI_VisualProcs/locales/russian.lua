bcfbcfc636ac2e183c268209e351b1a85cad6bbe
Pinya <800pin.ru@gmail.com>
2020-02-16 18:31:55 +0300

    remove BOM

--- ElvUI_VisualProcs/locales/russian.lua
+++ ElvUI_VisualProcs/locales/russian.lua
@@ -1,4 +1,4 @@
-﻿local AceLocale = LibStub:GetLibrary("AceLocale-3.0-ElvUI");
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0-ElvUI");
 local L = AceLocale:NewLocale("ElvUI", "ruRU");
 if(not L) then return; end
 
